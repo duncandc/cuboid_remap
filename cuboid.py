fixed lbox scaling

cuboid.py
@@ -3,8 +3,8 @@ cuboid class and related objects
 
 This code is a simple rewrite of Jordan Carlson and Martin White's code.
 
-I have removed the dependence on the vec3 class in the original implementation in
-favor of off-the-shelf numpy vector objects and functions.
+I have removed the dependence on the vec3 class in the original implementation
+in favor of off-the-shelf numpy vector objects and functions.
 """
 
 from __future__ import print_function, division
@@ -13,7 +13,7 @@ import numpy as np
 from cuboid_remap.utils import triple_scalar_product
 
 
-__all__=['Cuboid', 'Plane', 'Cell', 'unitCubeTest',]
+__all__ = ['Cuboid', 'Plane', 'Cell', 'unitCubeTest']
 __author__ = ['Duncan Campbell', 'see doc string note']
 
 
@@ -22,7 +22,7 @@ class Cuboid:
     cuboid remapping class.
     """
 
-    def __init__(self, u1=[1,0,0], u2=[0,1,0], u3=[0,0,1]):
+    def __init__(self, u1=[1, 0, 0], u2=[0, 1, 0], u3=[0, 0, 1]):
         """
         Parameters
         ----------
@@ -45,11 +45,11 @@ class Cuboid:
         u3 = np.atleast_1d(u3).astype('float64')
 
         if triple_scalar_product(u1, u2, u3) != 1:
-            msg = ("Invalid lattice vectors: u1 = %s, u2 = %s, u3 = %s" % (u1,u2,u3))
+            msg = ("Invalid lattice vectors: u1 = %s, u2 = %s, u3 = %s" % (u1, u2, u3))
             raise ValueError(msg)
         else:
-            s1 = np.dot(u1,u1)
-            s2 = np.dot(u2,u2)
+            s1 = np.dot(u1, u1)
+            s2 = np.dot(u2, u2)
             d12 = np.dot(u1, u2)
             d23 = np.dot(u2, u3)
             d13 = np.dot(u1, u3)
@@ -68,7 +68,7 @@ class Cuboid:
         self.n3 = self.e3/self.L3
         self.cells = []
 
-        v0 = np.array([0.0,0.0,0.0])
+        v0 = np.array([0.0, 0.0, 0.0])
         self.v = [v0,
                   v0 + self.e3,
                   v0 + self.e2,
@@ -133,13 +133,13 @@ class Cuboid:
         Transform coordinates
         """
         for c in self.cells:
-            if c.contains(x,y,z):
+            if c.contains(x, y, z):
                 x += c.ix
                 y += c.iy
                 z += c.iz
-                p = np.array([x,y,z])
+                p = np.array([x, y, z])
                 return (np.dot(p, self.n1), np.dot(p, self.n2), np.dot(p, self.n3))
-        msg = ("(%g, %g, %g) not contained in any cell" % (x,y,z))
+        msg = ("(%g, %g, %g) not contained in any cell" % (x, y, z))
         raise RuntimeError(msg)
 
     def InverseTransform(self, r1, r2, r3):
@@ -151,6 +151,7 @@ class Cuboid:
         x2 = fmod(p[1], 1) + (p[1] < 0)
         x3 = fmod(p[2], 1) + (p[2] < 0)
         return np.array([x1, x2, x3])
+
 
 class Plane:
     """
@@ -170,7 +171,7 @@ class Plane:
         self.a = n[0]
         self.b = n[1]
         self.c = n[2]
-        self.d = -1.0*np.dot(p,n)
+        self.d = -1.0*np.dot(p, n)
 
     def above(self, x, y, z):
         """
@@ -208,11 +209,11 @@ class Cell:
         determine if the cell contains a point
         """
         for f in self.faces:
-            if f.above(x,y,z) < 0:
+            if f.above(x, y, z) < 0:
                 return False
         return True
 
-    
+
 def unitCubeTest(P):
     """
     Detemrine if a unit cube is above, below, or intersecting a plane
@@ -223,21 +224,19 @@ def unitCubeTest(P):
 
     Returns
     -------
-    u : int 
+    u : int
         [+1, 0, -1] if the unit cube is above, below, or intersecting the plane.
     """
 
     above = 0
     below = 0
 
-    corners = [(0,0,0), (0,0,1), (0,1,0), (0,1,1),
-               (1,0,0), (1,0,1), (1,1,0), (1,1,1)]
-    for (a,b,c) in corners:
+    corners = [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1),
+               (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)]
+    for (a, b, c) in corners:
         s = P.above(a, b, c)
         if s > 0:
             above = 1
         elif s < 0:
             below = 1
     return above - below
-
-

remap.py
@@ -10,11 +10,11 @@ from cuboid_remap.cuboid import Cuboid
 from cuboid_remap.utils import triple_scalar_product, gcd, coprime_triples
 
 
-__all__=['remap', 'remap_Lbox', 'generate_lattice_vectors']
-__author__=['Duncan Campbell',]
+__all__ = ['remap', 'remap_Lbox', 'generate_lattice_vectors']
+__author__ = ['Duncan Campbell', ]
 
 
-def remap(coords, u1=[1,0,0], u2=[0,1,0], u3=[0,0,1]):
+def remap(coords, Lbox, u1=[1, 0, 0], u2=[0, 1, 0], u3=[0, 0, 1]):
     """
     Re-map coordinates in a periodic box.
 
@@ -23,6 +23,8 @@ def remap(coords, u1=[1,0,0], u2=[0,1,0], u3=[0,0,1]):
     coords : array_like
         numpy of array of shape (3,npts)
 
+    Lbox : float
+
     Returns
     -------
     new_coords : numpy.array
@@ -30,21 +32,21 @@ def remap(coords, u1=[1,0,0], u2=[0,1,0], u3=[0,0,1]):
     """
 
     coords = deepcopy(coords)
-    coords = np.atleast_1d(coords)
+    coords = np.atleast_1d(coords)/Lbox
 
-    assert np.shape(coords)[1]==(3)
+    assert np.shape(coords)[1] == (3)
 
     C = Cuboid(u1, u2, u3)
 
     # loop through each coordinate
     Npts = np.shape(coords)[0]
     for i in range(Npts):
-        coords[i,:] = C.Transform(coords[i,0], coords[i,1], coords[i,2])
+        coords[i, :] = C.Transform(coords[i, 0], coords[i, 1], coords[i, 2])
 
-    return coords
+    return coords*Lbox
 
 
-def remap_Lbox(u1=[1,0,0], u2=[0,1,0], u3=[0,0,1], Lbox=[1,1,1]):
+def remap_Lbox(u1=[1, 0, 0], u2=[0, 1, 0], u3=[0, 0, 1], Lbox=[1, 1, 1]):
     """
     Return re-mapped cuboid sides given a set of lattive vectors.
 
@@ -67,21 +69,21 @@ def remap_Lbox(u1=[1,0,0], u2=[0,1,0], u3=[0,0,1], Lbox=[1,1,1]):
     u2 = np.atleast_1d(u2)
     u3 = np.atleast_1d(u3)
 
-    assert np.shape(u1)==(3,)
-    assert np.shape(u2)==(3,)
-    assert np.shape(u3)==(3,)
+    assert np.shape(u1) == (3,)
+    assert np.shape(u2) == (3,)
+    assert np.shape(u3) == (3,)
 
     s1 = np.dot(u1, u1)
     s2 = np.dot(u2, u2)
-    
+
     d12 = np.dot(u1, u2)
     d23 = np.dot(u2, u3)
     d13 = np.dot(u1, u3)
-    
+
     alpha = -1.0*d12/s1
     gamma = -1.0*(alpha*d13 + d23)/(alpha*d12 + s2)
     beta = -1.0*(d13 + gamma*d12)/s1
-    
+
     e1 = u1
     e2 = u2 + alpha*u1
     e3 = u3 + beta*u1 + gamma*u2
@@ -112,11 +114,11 @@ def generate_lattice_vectors(max_int=5):
 
     Notes
     -----
-    Right now this uses a brute-force search method.  However, only 
+    Right now this uses a brute-force search method.  However, only
     matrices with co-prime triplet rows are conisdered.  I think
     this follows from the Quillen-Suslin Theorem.
     """
-    
+
     # find all coprime triples in the range [-max_int, max_int]
     d = coprime_triples(max_int, -1*max_int)
     triplets = d.keys()
@@ -127,7 +129,7 @@ def generate_lattice_vectors(max_int=5):
     # flatten list
     triplets = [item for sublist in triplets for item in sublist]
     num_triplets = len(triplets)
-    
+
     # create dictionary to store shear matrices
     m = {}
     num_remappings = 0
@@ -142,25 +144,25 @@ def generate_lattice_vectors(max_int=5):
                 u3 = np.array(triplets[k])
 
                 # check to see if matrix is unimodular
-                if triple_scalar_product(u1, u2, u3)==1:
+                if triple_scalar_product(u1, u2, u3) == 1:
 
                     # calculate cuboid side lengths
-                    L1, L2, L3 = np.sort(remap_Lbox(u1,u2,u3))[::-1]
+                    L1, L2, L3 = np.sort(remap_Lbox(u1, u2, u3))[::-1]
                     # use these lengths as the dictionary key
                     key = (L1, L2, L3)
 
                     # add this shear matrix to the dictionary
                     try:
-                        m[key].append([u1,u2,u3])
+                        m[key].append([u1, u2, u3])
                     # add if this geometry has not been found
                     except KeyError:
                         num_remappings += 1
-                        m[key] = [[u1,u2,u3]]
-            
+                        m[key] = [[u1, u2, u3]]
+
     return m
 
 
-    
+
 
 
 